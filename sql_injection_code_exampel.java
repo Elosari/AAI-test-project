c6a2713168808894527d6b2662ddb3a65cceaed7
Elosari <44024410+Elosari@users.noreply.github.com>
2023-11-18 15:23:59 +0100

    Update sql_injection_code_exampel.java

--- sql_injection_code_exampel.java
+++ sql_injection_code_exampel.java
@@ -1,5 +1,13 @@
-Scanner scanner = new Scanner(System.in);
-System.out.print("Gib einen Text ein: "); 
-String benutzerEingabe = scanner.nextLine();
-String sql = "SELECT spalten_name FROM meine_tabelle WHERE bedingung = ?";
-preparedStatement.setString(1, benutzerEingabe); // UNION SELECT username, password FROM user --
+import java.util.Scanner;
+
+public class EinfachesJavaProgramm {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Gib einen Text ein: "); 
+        String benutzerEingabe = scanner.nextLine();
+        String sql = "SELECT spalten_name FROM meine_tabelle WHERE bedingung = ?";
+        preparedStatement.setString(1, benutzerEingabe); // UNION SELECT username, password FROM user --
+        scanner.close();
+    }
+}

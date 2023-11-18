ae6a5b622965701772ea9785b43b4668bb8a2f69
Elosari <marcelsimmatgames@gmx.de>
2023-11-18 18:54:59 +0100

    hjvgk

--- sql_injection_code_exampel.java
+++ sql_injection_code_exampel.java
@@ -1,13 +1,23 @@
 import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-public class EinfachesJavaProgramm {
+public class sql_injection_code_exampel {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Gib einen Text ein: "); 
         String benutzerEingabe = scanner.nextLine();
-        String sql = "SELECT spalten_name FROM meine_tabelle WHERE bedingung = ?";
-        preparedStatement.setString(1, benutzerEingabe); // UNION SELECT username, password FROM user --
+
+        try (Connection conn = DriverManager.getConnection("", "user", "password");) {
+            PreparedStatement ps =  conn.prepareStatement("SELECT spalten_name FROM meine_tabelle WHERE bedingung = ?");
+            ps.setString(1, benutzerEingabe); // UNION SELECT username, password FROM user --
+        } catch (Exception e) {
+            System.err.println("Fehler beim Laden des JDBC-Treibers");
+            e.printStackTrace(); 
         scanner.close();
     }
+}   
 }

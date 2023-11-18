2d4a0d70a26a80930bf3e1fa1b75a166a805eb52
Elosari <44024410+Elosari@users.noreply.github.com>
2023-11-18 15:17:15 +0100

    Update and rename sql_injection_code_exampel.sql to sql_injection_code_exampel.java

---
+++ sql_injection_code_exampel.java
@@ -0,0 +1,5 @@
+Scanner scanner = new Scanner(System.in);
+System.out.print("Gib einen Text ein: ");
+String benutzerEingabe = scanner.nextLine();
+String sql = "SELECT spalten_name FROM meine_tabelle WHERE bedingung = ?";
+preparedStatement.setString(1, benutzerEingabe); // UNION SELECT username, password FROM user --
--- sql_injection_code_exampel.sql
+++
@@ -1,2 +0,0 @@
-query = 'SELECT article, title FROM our_texts WHERE title LIKE "%" + request.getParameter("searchString") + '%';
-SELECT article, title FROM our_texts WHERE title LIKE "%" UNION SELECT username, password FROM user -- %"

33befcbfa5f95d8bdcef73c0dc7014f4bf9f3840
Elosari <marcelsimmatgames@gmx.de>
2023-11-18 20:58:35 +0100

    lkjzhklu

--- sql_injection_code_exampel.java
+++ sql_injection_code_exampel.java
@@ -12,7 +12,8 @@ public class sql_injection_code_exampel {
         String benutzerEingabe = scanner.nextLine();
 
         try (Connection conn = DriverManager.getConnection("", "user", "password");) {
-            PreparedStatement ps =  conn.prepareStatement("SELECT spalten_name FROM meine_tabelle WHERE bedingung = ?");
+            PreparedStatement ps =  conn.prepareStatement(
+                "SELECT spalten_name FROM meine_tabelle WHERE bedingung = ?");
             ps.setString(1, benutzerEingabe); // UNION SELECT username, password FROM user --
         } catch (Exception e) {
             System.err.println("Fehler beim Laden des JDBC-Treibers");
